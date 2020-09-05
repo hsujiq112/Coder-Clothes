788bc462a3f7521bb74a8f36137ebcc9fe615461
Rares <manrares2001@gmail.com>
2020-09-05 16:09:55 +0300

    Initial Commit

---
+++ Coder-Clothes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coder-Clothes
+//
+//  Created by Rares Man on 05.09.2020.
+//  Copyright © 2020 Rares Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
